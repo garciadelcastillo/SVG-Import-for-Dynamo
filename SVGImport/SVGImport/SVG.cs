f0dd75acae05102e9011db870dc2f967780ad77a
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2017-08-07 15:58:03 -0400

    Project setup

---
+++ SVGImport/SVGImport/SVG.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Geometry;
+
+namespace SVGImport
+{
+    public class SVG
+    {
+        private SVG() { }
+
+    }
+}
